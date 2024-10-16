fb87d3544f28e0aede5c430177f4f741dc6de9de
RC_Student_lab <RC_Student_lab@10.0.0.94>
2024-10-16 17:35:13 +0000

    First Commit

---
+++ src/main/java/com/mycompany/icetask10/IceTask10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask10;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class IceTask10 {
+
+    public static void main(String[] args) {
+        
+    }
+}
